e2ea314be4b4d23dac1fcb6e0bea6e1a610c10b7
Naggyeop <lorentz0021@gmail.com>
2018-10-07 19:19:33 +0900

    typo
    #39

--- specials/SpacialSanctions.php
+++ specials/SpacialSanctions.php
@@ -44,7 +44,7 @@ class SpacialSanctions extends SpecialPage {
 			if ( $this->getUser()->isAnon () )
 				$output->addWikiText( '다음의 이유로 제재 절차 참여를 위한 조건이 맞지 않습니다. [[페미위키:제재 정책]]을 참고해 주세요.' );
 			else
-				$output->addWikiText( '다음의 이유로 현재 '.$this->getUser()->getName().' 님께서는 제재 절차에 잠여할 수 없습니다. [[페미위키:제재 정책]]을 참고해 주세요.' );
+				$output->addWikiText( '다음의 이유로 현재 '.$this->getUser()->getName().' 님께서는 제재 절차에 참여할 수 없습니다. [[페미위키:제재 정책]]을 참고해 주세요.' );
 
 			if ( count( $reason ) > 0 ) $output->addWikiText( '* '.implode( PHP_EOL.'* ', $reason ) );
 		}
