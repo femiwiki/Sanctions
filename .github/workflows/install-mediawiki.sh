d0213ece64b954ad6dc9ee779ac2829c11186542
lens0021 <lorentz0021@gmail.com>
2020-12-17 11:25:55 +0900

    Revert "Introduce Quibble"
    This reverts commit 70d4d6670180c8aed966fd3c6b98cdbffb10346d.

---
+++ .github/workflows/install-mediawiki.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -euo pipefail; IFS=$'\n\t'
+
+wget "https://github.com/wikimedia/mediawiki/archive/${MEDIAWIKI_VERSION}.zip" -q
+unzip -q "${MEDIAWIKI_VERSION}.zip" && rm "${MEDIAWIKI_VERSION}.zip"
+mv "mediawiki-${MEDIAWIKI_VERSION}" "${HOME}/mediawiki"
+cp -r "$GITHUB_WORKSPACE" "${HOME}/mediawiki/extensions/"
+
+composer install --prefer-dist --no-progress --no-interaction --working-dir "${HOME}/mediawiki"
+composer install --prefer-dist --no-progress --no-interaction --working-dir "${HOME}/mediawiki/extensions/Sanctions"
+
+php "${HOME}/mediawiki/maintenance/install.php" \
+  --pass password_for_testing \
+  --dbname testwiki \
+  --dbuser root \
+  --dbpass root \
+  --dbport "${MYSQL_PORT}" \
+  --scriptpath "/w" \
+  testwiki admin
+echo -e "\n\nrequire_once __DIR__ . '/includes/DevelopmentSettings.php';" >> "${HOME}/mediawiki/LocalSettings.php"
+echo -e "wfLoadExtension( 'Sanctions' ); >>" "${HOME}/mediawiki/LocalSettings.php"
