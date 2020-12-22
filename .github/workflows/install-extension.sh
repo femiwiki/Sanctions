81fc036d1728894f8fa992b4803aeb71117dac7b
lens0021 <lorentz0021@gmail.com>
2020-12-22 10:17:58 +0900

    Introduce Quibble

--- .github/workflows/install-extension.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euo pipefail; IFS=$'\n\t'
-
-wget "https://github.com/wikimedia/mediawiki-extensions-$1/archive/${MEDIAWIKI_VERSION}.zip" -q
-unzip -q "${MEDIAWIKI_VERSION}.zip" && rm "${MEDIAWIKI_VERSION}.zip"
-mv "mediawiki-extensions-$1-${MEDIAWIKI_VERSION}" "${HOME}/mediawiki/extensions/$1"
