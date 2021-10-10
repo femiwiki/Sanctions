fdd7dcb50b6aabafc973dc6e10936941da8130bd
Lens0021 <lorentz0021@gmail.com>
2021-10-10 22:16:20 +0900

    Convert sanctions board if not exist (#127)

---
+++ tests/selenium/config.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const Api = require('wdio-mediawiki/Api');
+
+class Config {
+  setVerifications(period, edits) {
+    browser.call(async () => {
+      const bot = await Api.bot();
+      await bot.edit(
+        'MediaWiki:sanctions-voting-right-verification-period',
+        '' + period
+      );
+      await bot.edit(
+        'MediaWiki:sanctions-voting-right-verification-edits',
+        '' + edits
+      );
+    });
+  }
+
+  set discussionPage(name) {
+    browser.call(async () => {
+      const bot = await Api.bot();
+      await bot.edit('MediaWiki:sanctions-discussion-page-name', name);
+    });
+  }
+}
+
+module.exports = new Config();
--- tests/selenium/pageobjects/sanctions.page.js
+++ tests/selenium/pageobjects/sanctions.page.js
@@ -5,8 +5,28 @@ class SanctionsPage extends Page {
   get reasonsDisabledParticipation() {
     return $('.sanctions-reasons-disabled-participation');
   }
+  get target() {
+    return $('#sanctions-target');
+  }
+  get forInsultingName() {
+    return $('#forInsultingName');
+  }
+  get submitButton() {
+    return $('#sanctions-submit-button');
+  }
+  get sanctions() {
+    return $('.sanctions');
+  }
+
   open(subpage) {
     super.openTitle('Special:Sanctions/' + subpage, { uselang: 'qqx' });
+  }
+  submit(target, isForInsultingName = false) {
+    this.target.setValue(target);
+    if (isForInsultingName) {
+      this.forInsultingName.click();
+    }
+    this.submitButton.click();
   }
 }
 module.exports = new SanctionsPage();
